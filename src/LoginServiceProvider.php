2130dda34b09a8ce24c950fdc430f68f25e77f12
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-02-06 14:36:20 +0800

    login package implementation

--- src/LoginServiceProvider.php
+++ src/LoginServiceProvider.php
@@ -16,12 +16,6 @@ class LoginServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadModuleCommands();
-        $this->loadRoutesFrom(__DIR__ . '/routes/login.php');
-        $this->loadViewsFrom(__DIR__ . '/views', 'login');
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->mergeConfigFrom(__DIR__ . '/config/login.php', 'login');
-
-        LoginResource::observe(LoginObserver::class);
 
         $this->publishes([
            __DIR__ . '/app/User.php' => app_path('User.php'),
@@ -34,8 +28,7 @@ class LoginServiceProvider extends ServiceProvider
 
     public function register()
     {
-        Event::listen(Quill::MODULE, RegisterLoginModule::class);
-        Event::listen(Quill::PERMISSION, RegisterLoginPermissionModule::class);
+
     }
 
     public function loadModuleCommands()
